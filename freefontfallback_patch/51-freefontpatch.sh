dcdbdedf594b3bc959ff851fa12b0c006b0f765a
Kethen <kwchuiaa@connect.ust.hk>
2018-03-09 17:21:07 +0800

    cleaning up scripts

--- freefontfallback_patch/51-freefontpatch.sh
+++ freefontfallback_patch/51-freefontpatch.sh
@@ -1,17 +1,22 @@
 #!/sbin/sh
 export PATH=/sbin
+FONTS_FOLDER=/system/fonts
+FONTS="FreeSans.ttf FreeSerif.ttf"
+XML="/system/etc/fonts.xml"
 if [ "$1" = "backup" ]
 then
-	cp /system/fonts/FreeSans.ttf /tmp/
-	cp /system/fonts/FreeSerif.ttf /tmp/
+	for f in $FONTS
+	do
+		cp $FONTS_FOLDER/$f /tmp/
+	done
 fi
 if [ "$1" = "restore" ]
 then
-	cp /tmp/FreeSans.ttf /system/fonts/
-	cp /tmp/FreeSerif.ttf /system/fonts/
-	chcon u:object_r:system_file:s0 /system/fonts/FreeSerif.ttf
-	chcon u:object_r:system_file:s0 /system/fonts/FreeSans.ttf
-	chmod 644 /system/fonts/FreeSans.ttf
-	chmod 644 /system/fonts/FreeSerif.ttf
-	sed -i '/<\/familyset>/i <family><font weight=\"400\" style=\"normal\">FreeSans.ttf<\/font><\/family><family><font weight=\"400\" style=\"normal\">FreeSerif.ttf<\/font><\/family>' /system/etc/fonts.xml
+	for f in $FONTS
+	do
+		cp /tmp/$f $FONTS_FOLDER
+		chcon u:object_r:system_file:s0 $FONTS_FOLDER/$f
+		chmod 644 $FONTS_FOLDER/$f
+		sed -i "/<\/familyset>/i <family><font weight=\"400\" style=\"normal\">$f<\/font><\/family>" $XML
+	done
 fi
